723f69443cc11cb1b8731cf7d244f4c81feb5ec6
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2022-07-08 13:13:31 +0600

    last

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -3,30 +3,31 @@ import Particle from './Particle';
 
 const Header = () => {
   return (
-    <div class="min-h-screen">
+    <div class="min-h-screen my-8">
       <div className='grid md:grid-cols-2'>
         <div class="hero-content text-center" data-aos="fade-right">
-          <div class="max-w-md">
+          <div class="max-w-md text-right">
             <h1 class="text-5xl font-bold text-secondary">Bar Council MCQ<br /><span className='text-4xl text-primary'>by Legal Fist</span></h1>
 
             <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button class="btn btn-primary animate-bounce text-white">Get Started</button>
+            <button class="btn btn-primary text-white">Get Started</button>
           </div>
         </div>
-        <div data-aos="fade-left">
-          <img src='https://pixabay.com/get/g918a79e1428dd8b639d82ca7523b0e1ccde2480e62aacefaa15740b497af5102b241b3fa4790513342a53fbbbe7d6aab72faca9267b94a2e521b4bec2bbad7c1d5bcfdaf171a758bca9af8ca111a81a3_640.png' alt="" />
+        <div data-aos="fade-left" className='flex justify-center items-center'>
+          <img className='w-2/3' src='https://www.thestatesman.com/wp-content/uploads/2019/11/judiciary.jpg' alt="" />
         </div>
       </div>
+      <div class="divider my-12">AND</div>
       <div className='grid md:grid-cols-2'>
-      <div data-aos="zoom-in-right">
-          <img src='https://pixabay.com/get/g918a79e1428dd8b639d82ca7523b0e1ccde2480e62aacefaa15740b497af5102b241b3fa4790513342a53fbbbe7d6aab72faca9267b94a2e521b4bec2bbad7c1d5bcfdaf171a758bca9af8ca111a81a3_640.png' alt="" />
+      <div data-aos="zoom-in-right" className='flex justify-center items-center'>
+          <img className='w-2/3' src='https://play-lh.googleusercontent.com/PoPXLWQT4mEwgAFevl_m7QgyctZZkbKX5_206dYRkFlZc6PH794I33uHR_rv4uyPGffO' alt="" />
         </div>
         <div class="hero-content text-center" data-aos="zoom-in-left">
-          <div class="max-w-md">
+          <div class="max-w-md text-left">
             <h1 class="text-5xl font-bold text-secondary">Judiciary MCQ<br /><span className='text-4xl text-primary'>by Legal Fist</span></h1>
 
             <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button class="btn btn-primary animate-bounce text-white">Get Started</button>
+            <button class="btn btn-primary text-white">Get Started</button>
           </div>
         </div>
         
